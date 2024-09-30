51fb97a6dd29fe43ba470a5d5a4443fbf712ce03
albasierra <146189049+albasierra@users.noreply.github.com>
2024-09-30 17:29:40 +0200

    Desafío Módulo 1

---
+++ Desafio.java
@@ -0,0 +1,19 @@
+package Modulo1;
+
+public class Desafio {
+    public static void main(String[] args) {
+        //nombre
+        String nombre = "Alba";
+
+        //Edad
+        int edad = 23;
+
+        //Ciudad de origen
+        String ciudad = "Málaga";
+
+        //Frase personalizada
+        String frase = "Ser o no ser, esa es la cuestión.";
+
+        System.out.println("Nombre: " + nombre + "\nEdad: " + edad + "\nCiudad de origen: " + ciudad + "\n\"" + frase + "\"");
+    }
+}
